9e791ba4cd230691aad23827125e19ed4a1340fc
Szymon Sobański <sobanski.s@gmail.com>
2012-10-25 21:53:12 +0200

    Makes tests check if parameters are being transferred between Ruby and C + some minor change in helpers.

--- spec/classifier_spec.rb
+++ spec/classifier_spec.rb
@@ -27,8 +27,9 @@ describe LinearScience::Classifier do
 		it 'should be able to receive custom parameters and train' do
 			add_examples_to_classifier(@classifier)
 			@classifier.parameters.e_epsilon = 0.1
-			@classifier.parameters.weights = {1 => 0.5}
-			@classifier.train.should be_an_instance_of LinearScience::Model
+			@classifier.parameters.weights = { 1 => 0.5 }
+			@classifier.train
+			@classifier.model.parameters["e_epsilon"].should == 0.1
 		end
 	end
 
@@ -40,12 +41,12 @@ describe LinearScience::Classifier do
 		end
 
 		it 'should return predicted label' do
-			@classifier.predict(@classification_example).should be_a_kind_of(Numeric)
+			@classifier.predict(@classification_example).should be_a_kind_of Numeric
 		end
 
 		it 'should not raise error if example dimension exceeds problem dimension' do
 			@oversized_example = { 1 => 1, 6 => 1 }
-			@classifier.predict(@oversized_example).should be_kind_of(Numeric)
+			@classifier.predict(@oversized_example).should be_kind_of Numeric 
 		end
 
 		it 'should raise error if example is ill-formed' do
@@ -63,22 +64,23 @@ describe LinearScience::Classifier do
 		end
 
 		it 'should be able to return probabilities' do
-			@classifier.predict_probability(@classification_example).should be_an_intance_of(Array)
+			@classifier.predict_probability(@classification_example).should be_an_instance_of Hash
 		end
 
-		it 'should return array of the length equal to number of classification classes' do
-			@classifier.predict_probability(@classification_example).length.should == @classifer.model.nr_class
+		it 'should return hash of the length equal to number of classification classes' do
+			@classifier.predict_probability(@classification_example).length.should == @classifier.model.nr_class
 		end
 
-		it 'should return array containing values between 0 and 1' do
-			@classifier.predict_probability(@classification_example).each do |prob|
-				(0..1).should cover(prob)
+		it 'should return hash containing values between 0 and 1' do
+			@classifier.predict_probability(@classification_example).values.each do |prob|
+				(0.0..1.0).should cover prob
 			end
 		end
 
-		it 'should raise an error if given example is malformed' do
-			@malformed_example = {1 => 1, 6 => 1}
-			expect{ @classifier.predict_probability(@malformed_example) }.to raise_error
+		it 'should raise an error if wrong kernel is used' do
+			@classifier.parameters.kernel = 1
+			@classifier.train
+			expect{ @classifier.predict_probability(@classification_example) }.to raise_error
 		end
 	end
 end
--- spec/helpers.rb
+++ spec/helpers.rb
@@ -3,7 +3,7 @@ Dir.entries("./lib/linear_science").grep(/.rb/).each{|f| require File.expand_pat
 
 module Helpers
 	def add_examples_to_classifier(classifier)
-		classifier.problem.add_example(1,  1 => 1, 3 => 1)
+		classifier.problem.add_example( 1, 1 =>  1, 3 =>  1)
 		classifier.problem.add_example(-1, 1 => -1, 3 => -1)
 	end
 end
